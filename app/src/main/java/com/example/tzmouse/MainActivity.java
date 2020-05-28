bea8a06d21761f91d4619b644c3248956d2b7d84
tmehulic1 <tmehulic1@etf.unsa.ba>
2020-05-28 21:18:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tzmouse/MainActivity.java
@@ -0,0 +1,194 @@
+package com.example.tzmouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private ViewHolder holder=new ViewHolder();
+
+    MqttAndroidClient mqttAndroidClient;
+
+    final String serverUri = "tcp://iot.eclipse.org:1883";
+    String clientId = "ExampleAndroidClient";
+    final String subscriptionTopic = "exampleAndroidTopic";
+    final String publishTopic = "exampleAndroidPublishTopic";
+    final String publishMessage = "Hello World!";
+
+
+    static class ViewHolder{
+        TextView x;
+        TextView y;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        holder.x=findViewById(R.id.x);
+        holder.y=findViewById(R.id.y);
+
+
+        mSensorManager= (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor=mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+        clientId = clientId + System.currentTimeMillis();
+
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+                if (reconnect) {
+//                    addToHistory("Reconnected to : " + serverURI);
+//                    // Because Clean Session is true, we need to re-subscribe
+                    subscribeToTopic();
+                } else {
+//                    addToHistory("Connected to: " + serverURI);
+                    System.out.println("Connected to: " + serverURI);;
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                System.out.println("CONN LOST");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+//                addToHistory("Incoming message: " + new String(message.getPayload()));
+                System.out.println("STIGLA PORUKA: "+new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+
+        try {
+            //addToHistory("Connecting to " + serverUri);
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                    subscribeToTopic();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    System.out.println("Failed to connect to: " + serverUri);
+                }
+            });
+
+        } catch (MqttException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    protected void onResume(){
+        super.onResume();
+        mSensorManager.registerListener(this,mSensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause(){
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x=event.values[0];
+        float y=event.values[1];
+
+        //TODO: OVDJE IDE PUBLISH MESSAGE DA SE PROSLIJEDE VRIJEDNOSTI OVE
+//        System.out.println("X--->"+x);
+//        System.out.println("Y--->"+y);
+//        if(Math.abs(x)>0.2){
+//            holder.x.setText(String.valueOf(x));
+//        }
+//        if(Math.abs(y)>0.2){
+//            holder.y.setText(String.valueOf(y));
+//        }
+    }
+
+    public void subscribeToTopic(){
+        try {
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+//                    addToHistory("Subscribed!");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+//                    addToHistory("Failed to subscribe");
+                }
+            });
+
+            // THIS DOES NOT WORK!
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    // message Arrived!
+                    System.out.println("Message: " + topic + " : " + new String(message.getPayload()));
+                }
+            });
+
+        } catch (MqttException ex){
+            System.err.println("Exception whilst subscribing");
+            ex.printStackTrace();
+        }
+    }
+
+    public void publishMessage(){
+
+        try {
+            MqttMessage message = new MqttMessage();
+            message.setPayload(publishMessage.getBytes());
+            mqttAndroidClient.publish(publishTopic, message);
+            System.out.println("Message published");
+            if(!mqttAndroidClient.isConnected()){
+                System.out.println(mqttAndroidClient.getBufferedMessageCount() + " messages in buffer.");
+            }
+        } catch (MqttException e) {
+            System.err.println("Error Publishing: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
