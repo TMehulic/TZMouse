577ed2ef73ec50a96c5fbf8aac5e1b3899477ba8
tmehulic1 <tmehulic1@etf.unsa.ba>
2020-06-21 17:06:10 +0200

    Filtered movement

--- app/src/main/java/com/example/tzmouse/MainActivity.java
+++ app/src/main/java/com/example/tzmouse/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
 
+
     private ViewHolder holder=new ViewHolder();
 
     MqttAndroidClient mqttAndroidClient;
@@ -61,8 +62,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     String publishLMBPressMessage="";
     String publishRMBPressMessage="";
 
-    private boolean LMBPressed=false;
-    private boolean RMBPressed=false;
 
 
 
@@ -72,6 +71,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         holder.leftClick=findViewById(R.id.leftClick);
         holder.rightClick=findViewById(R.id.rightClick);
@@ -283,19 +289,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         float y1=event.values[1];
         String x=null;
         String y=null;
-        if(Math.abs(x1)>0.02){
+        if(Math.abs(x1)>0.05){
             x=String.valueOf(x1);
         }else{
             x=String.valueOf(0);
         }
-        if(Math.abs(y1)>0.02){
+        if(Math.abs(y1)>0.05){
             y=String.valueOf(y1);
         }else{
             y=String.valueOf(0);
         }
-        publishMessage=x+","+y;
-        //TODO: UNCOMMENT LATER
-        publishMessage();
+        if(Double.parseDouble(x)>0.05 || Double.parseDouble(y)>0.05 ){
+            publishMessage=x+","+y;
+            //TODO: UNCOMMENT LATER
+            publishMessage();
+        }
+
+
 
     }
 
