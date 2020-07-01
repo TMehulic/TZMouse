5091cd23a66bf93503b01a8c9a0ed39c2765add5
tmehulic1 <tmehulic1@etf.unsa.ba>
2020-07-01 10:43:44 +0200

    Fixed layout positioning

--- app/src/main/java/com/example/tzmouse/MainActivity.java
+++ app/src/main/java/com/example/tzmouse/MainActivity.java
@@ -76,13 +76,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         holder.leftClick=findViewById(R.id.leftClick);
